6ceeb234f044025bfcf3968063593d26f0fa21da
bi-yechao <1911789242@qq.com>
2022-03-29 22:22:41 +0800

    添加两个Python文件和生成的图表，练习了pygal中工具提示的使用

--- python_repos.py
+++ python_repos.py
@@ -1,13 +1,43 @@
 import requests
+import pygal
+from  pygal.style import LightColorizedStyle as LCS,LightenStyle as LS
 
-#github的searchapi的使用有改变跟书上不同
-url = 'https://api.github.com/repositories?q=language:python&sort=stars'
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 r = requests.get(url)
 print("Status code:",r.status_code)
 
+#api响应存储在变量中
 response_dict = r.json()
 
-#response_dict是包含多个字典的列表，书上直接访问response_dict.keys()是错的？
-print(response_dict[0].keys())
+repo_dicts = response_dict['items']
 
-print('Total repositories:'+response_dict[0]['total_count'])
+names,plot_dicts = [],[]
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+
+    plot_dict = {
+        'value':repo_dict['stargazers_count'],
+        'label':repo_dict['description'],
+        'xlink':repo_dict['html_url']
+    }
+    plot_dicts.append(plot_dict)
+
+#可视化
+my_style = LS('#333366',base_style=LCS)
+
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config,style=my_style)
+chart.title = 'Most-Starred Python Projects on Github'
+chart.x_labels = names
+
+chart.add('',plot_dicts)
+chart.render_to_file('python_repos.svg')
